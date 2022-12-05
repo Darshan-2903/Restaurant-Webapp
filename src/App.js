f7dbda16e6f9acaa61d9b09d6f888af6643c28d5
Darshan Mahajan <darshantmahajan29@gmail.com>
2022-12-05 11:04:46 +0530

    app completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { AnimatePresence } from 'framer-motion';
-import { Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes} from "react-router-dom";
 import { CreateContainer, Header, MainContainer } from './components';
 import { useStateValue } from "./context/StateProvider";
 import { getAllFoodItems } from "./utils/firebaseFunctions";
@@ -31,11 +31,13 @@ const App = () => {
      <Header/>
 
      <main className='mt-14 md:mt-20 px-4 md:px-16 py-4 w-full'>
-      <Routes>
-        {/* <Routes basename={process.env.PUBLIC_URL}> */}
+      <BrowserRouter basename="/Restaurant-Webapp">
+       <Routes>
+
         <Route path="/*" element={<MainContainer/>}/>
         <Route path="/createItem" element={<CreateContainer/>}/>
-        </Routes>
+       </Routes>
+        </BrowserRouter>
      </main>
     </div>
   </AnimatePresence>
